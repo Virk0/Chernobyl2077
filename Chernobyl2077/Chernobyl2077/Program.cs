c39e8e2b8a4af347afe7fc01d4a4cf09c7ab93c2
Virk0 <virko.veelmaa@tptlive.ee>
2017-12-11 11:39:17 +0200

    Starting point

--- Chernobyl2077/Chernobyl2077/Program.cs
+++ Chernobyl2077/Chernobyl2077/Program.cs
@@ -10,6 +10,38 @@ namespace Chernobyl2077
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Mr bean has finally arrived in chernobyl");
+            Console.WriteLine("Mr.B: It's even worse than i thought");
+            Console.WriteLine("Mr.B: This place is in ruins and radioactive");
+            Console.WriteLine("Suddenly a squatting person walks up to him and speaks gibberish to him");
+            Console.WriteLine("???: Что ты здесь делаешь?");
+            Console.WriteLine("Mr Bean doesn't understand a single word this guy said");
+            Console.WriteLine("Mr.B: Wat?");
+            Console.WriteLine("???: Ты можешь говорить по русски?");
+            Console.WriteLine("Mr Bean still didn't undestand anything this being said");
+            Console.WriteLine("Bean tries to move past him");
+            Console.WriteLine("???:Где вы думаете, что собираетесь");
+            Console.WriteLine( "You enter in combat with the squatting man, who can only speak gibberish!");
+            Combat1:
+            Console.WriteLine("Insert Combat system");
+            if (Outcome == "Player won")
+            {
+                Console.WriteLine("You proceed to a temporary station located near you");
+
+            }
+            if (Outcome == "Player lost")
+            {
+                Console.WriteLine("Do you wish to try again?\n1.Yes\n2.No");
+                string choise2 = Console.ReadLine();
+                if (choise2 == "1")
+                {
+                    Console.WriteLine("Returning");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    goto Combat1;
+                }
+            }
         }
     }
 }
