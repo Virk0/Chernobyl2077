85dc618f9529bd0dd037ae1237a963f507d12249
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-12-11 09:34:13 +0200

    Art folder

--- Chernobyl2077/Chernobyl2077/Program.cs
+++ Chernobyl2077/Chernobyl2077/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Chernobyl2077
 {
@@ -10,6 +11,7 @@ namespace Chernobyl2077
     {
         static void Main(string[] args)
         {
+            using(StreamReader Squid_image=new StreamReader("C:\Users\opilane\Documents\GitHub\Chernobyl2077\Art\\Squiddab.txt"))
         }
     }
 }
