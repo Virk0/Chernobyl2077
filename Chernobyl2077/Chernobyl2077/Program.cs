d7afc04a4e0adc55018c85c470ec5ac6afbcf124
Virk0 <virko.veelmaa@tptlive.ee>
2017-12-12 13:02:59 +0200

    k

--- Chernobyl2077/Chernobyl2077/Program.cs
+++ Chernobyl2077/Chernobyl2077/Program.cs
@@ -10,7 +10,13 @@ namespace Chernobyl2077
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
 
+            if (rand.Next(0, 2) == 0)
+                Console.WriteLine("You died");
+            else
+                Console.WriteLine("You live");
+            Console.ReadLine();
         }
     }
 }
