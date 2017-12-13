87833d772214d925aa335e750a765d3aa803b501
Si1mT <33309403+Si1mT@users.noreply.github.com>
2017-12-13 10:18:52 +0200

    trying to randomize goblin stats

--- Chernobyl2077/Chernobyl2077/Goblin.cs
+++ Chernobyl2077/Chernobyl2077/Goblin.cs
@@ -7,11 +7,13 @@ using System.IO;
 
 namespace Chernobyl2077
 {
-    public class Goblin : Entity
+    public class Goblin : Entity  
     {
         //string path = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName,"");
 
-        public int GoblinHP = 20;
+        Random randomValues = new Random();
+        int xd = (randomValues.Next(5, 20));
+        public int GoblinHP = ;
         public int GoblinAP = 10;
         MrBean mr = new MrBean();
         //string[] lines = File.ReadAllLines(path + "\\" + konto.Name + ".txt");
